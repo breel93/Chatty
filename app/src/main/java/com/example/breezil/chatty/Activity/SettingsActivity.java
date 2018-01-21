7aedc8d65111278a20e1925aef7f05b23cfaa3fd
breel93 <kolajubril@gmail.com>
2018-01-21 18:12:42 +0100

    settings activities

--- app/src/main/java/com/example/breezil/chatty/Activity/SettingsActivity.java
+++ app/src/main/java/com/example/breezil/chatty/Activity/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,11 +14,29 @@ import android.widget.TextView;
 
 import com.example.breezil.chatty.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private TextView mLogout;
     private Toolbar mToolbar;
+
+    private CardView mUserInfo;
+    private CircleImageView mUserImage;
+    private TextView mUserName, mUserStatus;
+
+    private DatabaseReference mUserRef;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,34 @@ public class SettingsActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.settingBar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Settings");
+
+        mUserInfo = (CardView) findViewById(R.id.userInfoCard);
+        mUserImage = (CircleImageView) findViewById(R.id.userSettingsImage);
+        mUserName = (TextView) findViewById(R.id.userSettingsName);
+        mUserStatus = (TextView) findViewById(R.id.userSettingStatus);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        String Uid = mAuth.getCurrentUser().getUid();
+
+        mUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Uid);
+
+
+        userInfo();
+
+
+        mUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this,SettupActivity.class));
+            }
+        });
+
+
+
+
+
+
 
         mLogout = (TextView) findViewById(R.id.logOutText);
         mLogout.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +126,42 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void userInfo() {
+
+        mUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                final String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+
+                mUserName.setText(name);
+                mUserStatus.setText(status);
+
+                if(!thumb_image.equals("default")){
+                    Picasso.with(SettingsActivity.this).load(thumb_image).networkPolicy(NetworkPolicy.OFFLINE)
+                            .placeholder(R.drawable.default_avatar).into(mUserImage, new Callback() {
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError() {
+                            Picasso.with(SettingsActivity.this).load(thumb_image).placeholder(R.drawable.default_avatar).into(mUserImage);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     //Creating the option menu
